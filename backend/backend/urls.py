aa01b883757db241c6be54c84903c641de48c802
chizzyjpg <chizzyjpg@gmail.com>
2025-10-25 18:21:18 -0300

    xd

---
+++ backend/backend/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from users import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', views.list_users),
+    path('users/create/', views.create_user),
+]
